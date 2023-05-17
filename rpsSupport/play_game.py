f423c281c72cd01827292270bbabf0fa7c56b3c4
Michael Cheng <michaelcheng@MacBook-Pro-3.local>
2023-05-17 10:57:06 -0700

    modified determine_winner and play_game function

--- rpsSupport/play_game.py
+++ rpsSupport/play_game.py
@@ -1,12 +1,7 @@
 
 import random
 
-rule_dict = {'rock':['scissors'],
-           'paper':['rock'],
-           'scissors':['paper']}
-
-
-def play_game(player_throw:str, computer_throw:str, rule_dict:dict) -> str:
+def determine_winner(player_throw:str, computer_throw:str, rule_dict:dict) -> str:
     player_throw = player_throw.lower()
     if player_throw not in rule_dict.keys():
         return('')
@@ -17,19 +12,12 @@ def play_game(player_throw:str, computer_throw:str, rule_dict:dict) -> str:
     else:
         return('T')
 
-if __name__=='__main__':
-    while True:
-        player_throw = input('rock, paper, or scissors?: ')
-        computer_throw = random.choice(list(rule_dict.keys()))
-        print('computer: ' + computer_throw)
-        outcome = play_game(player_throw, computer_throw, rule_dict)
-        if outcome=='P':
-            print('Player Wins')
-            break
-        elif outcome=='C':
-            print('Computer Wins')
-            break
-        elif outcome=='T':
-            print('Tie, Throw Again')
-        else:
-            print('Player Throw Invalid, Try Again')
+def play_game() -> str:
+    rule_dict = {'rock': ['scissors'],
+                 'paper': ['rock'],
+                 'scissors': ['paper']}
+    player_throw = input('rock, paper, or scissors?: ')
+    computer_throw = random.choice(list(rule_dict.keys()))
+    print('computer: ' + computer_throw)
+    outcome = determine_winner(player_throw, computer_throw, rule_dict)
+    return(outcome)
