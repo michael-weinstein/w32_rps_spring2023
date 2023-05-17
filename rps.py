366d50c5053bce2d4779eefbd3ce9242dade1c4c
Jack Dodson <jackdodson@ucla.edu>
2023-05-17 10:38:41 -0700

    added get_winner function

--- rps.py
+++ rps.py
@@ -15,7 +15,7 @@ def get_cpu_throw(objects: List[str]) -> str:
     return(objects[throw])
 
 def get_player_throw(objects: List[str]) -> str:
-    throw = input('Throw: ').lower()
+    throw = input('Enter Throw: ').lower()
     if throw in objects:
         return(throw)
     else:
@@ -24,6 +24,14 @@ def get_player_throw(objects: List[str]) -> str:
         else:
             print('Please enter valid object: ' + str(objects))
             return None
+
+def get_winner(player_throw: str, cpu_throw: str) -> str:
+    if cpu_throw == player_throw:
+        return('Tie') 
+    elif player_throw in winner_dict[cpu_throw]:
+        return('Player')
+    else:
+        return('CPU')
 
 if __name__ == '__main__':
     player_wins = 0
@@ -40,13 +48,15 @@ if __name__ == '__main__':
             ties = 0
             cpu_wins = 0
             player_wins = 0
-        elif cpu_throw == player_throw:
-            print('Tie') 
-            ties += 1   
-        elif player_throw in winner_dict[cpu_throw]:
-            print('Player wins')
+        winner = get_winner(player_throw, cpu_throw)
+        if(winner == 'Tie'):
+            print('TIE')
+            ties += 1
+        elif(winner == 'Player'):
+            print('PLAYER WINS')
             player_wins += 1
         else:
-            print('CPU wins')
+            print('CPU WINS')
             cpu_wins += 1
+        
     print('Total wins: CPU: %d PLAYER: %d TIE: %d'%(cpu_wins, player_wins, ties))
