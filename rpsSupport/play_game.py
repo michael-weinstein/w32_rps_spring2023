b899708b1704372edce2ffd8eea3800edca47b30
Michael Cheng <michaelcheng@MacBook-Pro-3.local>
2023-05-17 10:23:17 -0700

    Move requirements.txt, added function to play_game.py

--- rpsSupport/play_game.py
+++ rpsSupport/play_game.py
@@ -6,16 +6,29 @@ rule_dict = {'rock':['scissors'],
            'scissors':['paper']}
 
 
-while True:
-    player_throw = input('rock, paper, or scissors?: ')
-    computer_throw = random.choice(['rock','paper','scissors'])
-    print('computer: '+computer_throw)
+def play_game(player_throw:str, computer_throw:str, rule_dict:dict):
+    if player_throw not in rule_dict.keys():
+        print('Player Throw Invalid')
+        return(None)
+    if computer_throw not in rule_dict.keys():
+        print('Computer Throw Invalid')
+        return(None)
     if computer_throw in rule_dict[player_throw]:
         print('Player Wins')
-        break
+        return('Player')
     elif player_throw in rule_dict[computer_throw]:
         print('Computer Wins')
-        break
+        return('Computer')
     else:
-        print('Throw Again')
+        print('Tie')
+        return(None)
+
+if __name__=='__main__':
+    while True:
+        player_throw = input('rock, paper, or scissors?: ')
+        computer_throw = random.choice(['rock', 'paper', 'scissors'])
+        print('computer: ' + computer_throw)
+        outcome = play_game(player_throw, computer_throw, rule_dict)
+        if outcome:
+            break
 
