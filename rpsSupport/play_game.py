13df4d710e14aa46e2758ada95f818e702576949
Michael Cheng <michaelcheng@MacBook-Pro-3.local>
2023-05-17 10:40:19 -0700

    Update play_game.py

--- rpsSupport/play_game.py
+++ rpsSupport/play_game.py
@@ -6,29 +6,30 @@ rule_dict = {'rock':['scissors'],
            'scissors':['paper']}
 
 
-def play_game(player_throw:str, computer_throw:str, rule_dict:dict):
+def play_game(player_throw:str, computer_throw:str, rule_dict:dict) -> str:
+    player_throw = player_throw.lower()
     if player_throw not in rule_dict.keys():
-        print('Player Throw Invalid')
-        return(None)
-    if computer_throw not in rule_dict.keys():
-        print('Computer Throw Invalid')
-        return(None)
+        return('')
     if computer_throw in rule_dict[player_throw]:
-        print('Player Wins')
-        return('Player')
+        return('P')
     elif player_throw in rule_dict[computer_throw]:
-        print('Computer Wins')
-        return('Computer')
+        return('C')
     else:
-        print('Tie')
-        return(None)
+        return('T')
 
 if __name__=='__main__':
     while True:
         player_throw = input('rock, paper, or scissors?: ')
-        computer_throw = random.choice(['rock', 'paper', 'scissors'])
+        computer_throw = random.choice(list(rule_dict.keys()))
         print('computer: ' + computer_throw)
         outcome = play_game(player_throw, computer_throw, rule_dict)
-        if outcome:
+        if outcome=='P':
+            print('Player Wins')
             break
-
+        elif outcome=='C':
+            print('Computer Wins')
+            break
+        elif outcome=='T':
+            print('Tie, Throw Again')
+        else:
+            print('Player Throw Invalid, Try Again')
