9b3b08ac1a53b66d946247330a430acbbf08f808
Michael Weinstein <michael.weinstein@ucla.edu>
2023-05-17 22:58:48 -0700

    Moved play a round to a function. Cleaned up requirements.txt

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 import rpsSupport
 
+
+def playARound():
+    outcome = rpsSupport.play_game.play_game()
+    if outcome == 'P':
+        print('Player wins')
+    elif outcome == 'C':
+        print('Computer wins')
+    elif outcome == 'T':
+        print('Tie')
+    else:
+        print('Player Throw Invalid')
+
+
 if __name__=='__main__':
     num_games = int(input('Enter number of games to play: '))
     for i in range(num_games):
-        outcome = rpsSupport.play_game.play_game()
-        if outcome == 'P':
-            print('Player wins')
-        elif outcome == 'C':
-            print('Computer wins')
-        elif outcome == 'T':
-            print('Tie')
-        else:
-            print('Player Throw Invalid')
+        playARound()
+
             
